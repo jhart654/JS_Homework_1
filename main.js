8bd8c20d864754e3ae4c7b1bcda072c3c89232e0
jhart654 <143129762+jhart654@users.noreply.github.com>
2024-03-17 21:40:10 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -28,3 +28,48 @@ function replaceEvens(arr) {
 }
 
 console.log(replaceEvens(arr))
+
+// Codewars #1
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+function problem(a){
+    if(a === Number(a)){
+        return (a * 50) + 6;
+    } else {
+        return "Error";
+    }
+}       
+
+console.log(problem(4))
+
+// Codewars #2
+// Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b) {
+    if (b === 'True'){
+        return true
+    } else if (b === 'False') {
+        return false
+    }
+}
+
+console.log(booleanToString('False'))
+
+// Codewars #3
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+function greet(name) {
+    console.log(`Hello ${name}, how are you doing today?`)
+}
+
+console.log(greet('Jordan'))
+
+// Codewars #4
+// Write a function to split a string and convert it into an array of words.
+
+function stringToArray(s) {
+    let list = s.split(' ')
+    console.log(list)
+}
+
+console.log(stringToArray("I love arrays"))
